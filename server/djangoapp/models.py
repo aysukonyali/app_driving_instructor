6ae0db460d2c6bf4753124ff4bde0366385631e0
aysukonyali <aysu.konyali@gmail.com>
2022-08-27 17:56:06 +0000

    changes

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,27 +1,16 @@
 from django.db import models
 from django.utils.timezone import now
+from django.core import serializers 
+import uuid
+import json
+
+class UserFeedback:
+    def __init__(self, name, surname, feedback, email):
+        self.name = name
+        self.surname = surname
+        self.email = email
+        self.feedback = feedback
 
 
-# Create your models here.
-
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
-
-
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
-# - Name
-# - Dealer id, used to refer a dealer created in cloudant database
-# - Type (CharField with a choices argument to provide limited choices such as Sedan, SUV, WAGON, etc.)
-# - Year (DateField)
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
-
-
-# <HINT> Create a plain Python class `CarDealer` to hold dealer data
-
-
-# <HINT> Create a plain Python class `DealerReview` to hold review data
+    def __str__(self):
+        return "Feedback: " + self.feedback
--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,34 +1,48 @@
 import requests
 import json
-# import related models here
+from .models import UserFeedback
 from requests.auth import HTTPBasicAuth
 
-
-# Create a `get_request` to make HTTP GET requests
-# e.g., response = requests.get(url, params=params, headers={'Content-Type': 'application/json'},
-#                                     auth=HTTPBasicAuth('apikey', api_key))
-
-
-# Create a `post_request` to make HTTP POST requests
-# e.g., response = requests.post(url, params=kwargs, json=payload)
-
-
-# Create a get_dealers_from_cf method to get dealers from a cloud function
-# def get_dealers_from_cf(url, **kwargs):
-# - Call get_request() with specified arguments
-# - Parse JSON results into a CarDealer object list
-
-
-# Create a get_dealer_reviews_from_cf method to get reviews by dealer id from a cloud function
-# def get_dealer_by_id_from_cf(url, dealerId):
-# - Call get_request() with specified arguments
-# - Parse JSON results into a DealerView object list
+def get_request(url, **kwargs):
+    print(kwargs)
+    print("GET from {} ".format(url))
+    try:
+        # Call get method of requests library with URL and parameters
+        response = requests.get(url, headers={'Content-Type': 'application/json'},
+                                    params=kwargs)
+    except:
+        # If any error occurs
+        print("Network exception occurred")
+    status_code = response.status_code
+    print("With status {} ".format(status_code))
+    json_data = json.loads(response.text)
+    return json_data
 
 
-# Create an `analyze_review_sentiments` method to call Watson NLU and analyze text
-# def analyze_review_sentiments(text):
-# - Call get_request() with specified arguments
-# - Get the returned sentiment label such as Positive or Negative
+def get_feedback(url):
+    results = []
+    json_result = get_request(url)
+    if json_result:
+        feedbacks = json_result["body"]
+        for feedback in feedbacks:
+            fb_doc = dealer["doc"]
+            fb_obj = UserFeedback(
+                name=fb_doc["name"],
+                surname=fb_doc["surname"],
+                email=fb_doc["email"],
+                feedback=fb_doc["feedback"]  
+            )
+            results.append(fb_obj)
+    return results
 
 
+def post_request(url, payload, **kwargs):
+    print(kwargs)
+    print("POST to {} ".format(url))
+    print(payload)
+    response = requests.post(url, params=kwargs, json=payload)
+    status_code = response.status_code
+    print("With status {} ".format(status_code))
+    json_data = json.loads(response.text)
+    return json_data
 
